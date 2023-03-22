5c2f2f8c0a9045dc513e28da51e1c7b0be1ea82f
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-22 15:43:39 +0330

    test entering name and email and submiting the form for UserForm component

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -14,3 +14,26 @@ test('it shows two inputs and a button', () => {
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
 });
+
+test('it calls onUserAdd when the form is submitted', () => {
+    const argList = [];
+    const callBack = (...args) => {
+        argList.push(args);
+    };
+
+    render(<UserForm onUserAdd={callBack} />);
+
+    const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+    user.click(nameInput);
+    user.keyboard('ehsan');
+
+    user.click(emailInput);
+    user.keyboard('ehsan@gmail.com');
+
+    const button = screen.getByRole('button');
+    user.click(button);
+
+    expect(argList).toHaveLength(1);
+    expect(argList[0][0]).toEqual({ name: 'ehsan', email: 'ehsan@gmail.com' });
+});
