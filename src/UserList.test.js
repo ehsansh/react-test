9205096fed2f7c34a69c1c3f4da20731a6da2328
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-23 14:08:34 +0330

    add renderComponent function to follow dry rules

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,28 +1,31 @@
 import { render, screen, act, within } from '@testing-library/react';
 import UserList from './UserList';
 
-test('render one row per user', () => {
-    //render component
+function renderComponent() {
     const users = [
         { name: 'ehsan', email: 'ehsan@gmail.com' },
         { name: 'josh', email: 'josh@gmail.com' },
     ];
-    const { container } = render(<UserList users={users} />);
+    render(<UserList users={users} />);
+    return {
+        users,
+    };
+}
+
+test('render one row per user', () => {
+    //render component
+    renderComponent();
 
     //find all the rows in the table
-    // const rows = within(screen.getByTestId('users')).getAllByRole('row');
-    const rows = container.querySelectorAll('tbody tr');
+    const rows = within(screen.getByTestId('users')).getAllByRole('row');
+    // const rows = container.querySelectorAll('tbody tr');
 
     //assertion: correct number of rows in the table
     expect(rows).toHaveLength(2);
 });
 
 test('render email and name of each user', () => {
-    const users = [
-        { name: 'ehsan', email: 'ehsan@gmail.com' },
-        { name: 'josh', email: 'josh@gmail.com' },
-    ];
-    const { container } = render(<UserList users={users} />);
+    const { users } = renderComponent();
 
     for (let user of users) {
         const name = screen.getByRole('cell', {
