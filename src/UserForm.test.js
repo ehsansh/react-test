c8875209f52e811f296f8d3e94b84593481d1727
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-23 15:04:09 +0330

    write test to check form inputs are empty after submission

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -48,3 +48,32 @@ test('it calls onUserAdd when the form is submitted', () => {
         email: 'ehsan@gmail.com',
     });
 });
+
+test('empty the two inputs when the form is submitted', () => {
+    render(<UserForm onUserAdd={() => {}} />);
+    const nameInput = screen.getByRole('textbox', {
+        name: /name/i,
+    });
+    const emailInput = screen.getByRole('textbox', {
+        name: /email/i,
+    });
+
+    const button = screen.getByRole('button');
+
+    user.click(nameInput);
+    act(() => {
+        user.keyboard('ehsan');
+    });
+
+    user.click(emailInput);
+    act(() => {
+        user.keyboard('ehsan@gmail.com');
+    });
+
+    act(() => {
+        user.click(button);
+    });
+
+    expect(nameInput).toHaveValue('');
+    expect(emailInput).toHaveValue('');
+});
