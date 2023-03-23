c1fd6c88dfd8672afd2e110ed4d8f864fdceb4ac
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-23 14:53:05 +0330

    write tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('can receive a new user and show it on a list', () => {
+    render(<App />);
+
+    const nameInput = screen.getByRole('textbox', {
+        name: /name/i,
+    });
+    const emailInput = screen.getByRole('textbox', {
+        name: /email/i,
+    });
+
+    const button = screen.getByRole('button');
+
+    user.click(nameInput);
+    act(() => {
+        user.keyboard('ehsan');
+    });
+
+    user.click(emailInput);
+    act(() => {
+        user.keyboard('ehsan@gmail.com');
+    });
+
+    act(() => {
+        user.click(button);
+    });
+
+    const name = screen.getByRole('cell', {
+        name: 'ehsan',
+    });
+    const email = screen.getByRole('cell', {
+        name: 'ehsan@gmail.com',
+    });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
 });
