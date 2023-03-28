bcf80ad14a4d8e8e35b9d52f934833b95594661e
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-28 19:10:13 +0330

    write test for typing in email input

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('inputs should be initially empty', () => {
@@ -12,4 +13,14 @@ test('inputs should be initially empty', () => {
 
     const confirmPasswordInput = screen.getByLabelText(/confirm password/i);
     expect(confirmPasswordInput.value).toBe('');
+});
+
+test('should be able to type an email', () => {
+    render(<App />);
+
+    const emailInput = screen.getByRole('textbox', {
+        name: /emai/i,
+    });
+    userEvent.type(emailInput, 'ehsan@gmail.com');
+    expect(emailInput.value).toBe('ehsan@gmail.com');
 });
