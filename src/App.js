1d8030ca69423df767af5c3538229eb316ed7c1d
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-28 15:57:16 +0330

    add bootstrap and add email input to the app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return <div></div>;
+    return (
+        <div className='container my-5'>
+            <form>
+                <div className='mb-3'>
+                    <label htmlFor='email' className='form-label'>
+                        Email address
+                    </label>
+                    <input
+                        type='email'
+                        id='email'
+                        name='email'
+                        className='form-control'
+                    />
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default App;
