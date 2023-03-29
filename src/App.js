1512658906caecadd9b686e91a726a445372f704
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-29 18:19:50 +0330

    add card to app component for manual testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Card
+                name='john'
+                phone='123'
+                email='john@example.com'
+                image={{
+                    url: 'https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+                    alt: 'cute cat',
+                }}
+                favoured={false}
+            />
+        </div>
+    );
 }
 
 export default App;
