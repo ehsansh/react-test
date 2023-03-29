8693994d20cb68d23833a5ca8f00cd5f3e43a1ef
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-29 20:28:53 +0330

    add axios and fetch cats data from server on localhost

--- src/components/Pets/Pets.js
+++ src/components/Pets/Pets.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Filter from '../Filter/Filter';
 import Cards from '../Cards/Cards';
 import './Pets.css';
-import cats from '../../mocks/cats.json';
+
 const Pets = () => {
+    const [cats, setCats] = useState([]);
+
+    const fetchCats = async () => {
+        const response = await axios.get('http://localhost:4000/cats');
+        setCats(response.data);
+    };
+
+    useEffect(() => {
+        fetchCats();
+    }, []);
+
     return (
         <div className='container'>
             <div className='app-container'>
